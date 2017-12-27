7354cf111c4b467c8533363ea1dd8b2c41e5c7bb
Iliyass Hamza <iliyass.hamza@gmail.com>
2017-12-27 09:17:09 +0000

    [REFACTOR] moved videos module

--- backend/videos/urls.py
+++ backend/videos/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 from rest_framework_mongoengine.routers import DefaultRouter
 
-from videos.urls import module_urls as VideoUrls
+from .views import VideosViewSet
 
 
 router = DefaultRouter()
+router.register('videos', VideosViewSet, base_name='videos_view_set')
 
-urlpatterns = router.urls + VideoUrls
+module_urls = router.urls
